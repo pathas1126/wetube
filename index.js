4fccef042326287791f93908afd574020958d22b
unknown <pathas@naver.com>
2019-10-07 21:55:49 +0900

    Enter

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hi");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+// const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from my ass");
+
+const handleProfile = (req, res) => res.send("you are on my profile");
+
+app.use(cookieParser());
+
+app.use(bodyParser.json({ extended: true }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.get("/", middle, handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(4000, handleListening);
