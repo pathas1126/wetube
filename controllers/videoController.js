efe68961e48372b103c75e6758e128f97fb6faeb
unknown <pathas@naver.com>
2019-10-11 18:31:42 +0900

    enter

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,21 +4,21 @@ export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req; // it is same as const searchingBy = req.query.term
-  res.render("Search", { pageTitle: "Search", searchingBy });
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
 
 export const videos = (req, res) =>
-  res.render("Videos", { pageTitle: "Videos" });
+  res.render("videos", { pageTitle: "Videos" });
 
 export const upload = (req, res) =>
-  res.render("Upload", { pageTitle: "Upload" });
+  res.render("upload", { pageTitle: "Upload" });
 
 export const videoDetail = (req, res) =>
-  res.render("Video Detail", { pageTitle: "Video Detail" });
+  res.render("videoDetail", { pageTitle: "Video Detail" });
 
 export const editVideo = (req, res) =>
-  res.render("Edit Video", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 
 export const deleteVideo = (req, res) =>
-  res.render("Delete Video", { pageTitle: "Delete Video" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
 //first arguement of 'render' function is template, second is object including informations that you wanna add to template
